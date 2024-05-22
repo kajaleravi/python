31b3d652560219bb9eaf8754bf96cc8125c0c4c5
ravi kajale <kajaleravi@outlook.com>
2024-05-22 12:36:02 +0530

    practice questions

---
+++ practice/pratice.py
@@ -0,0 +1,34 @@
+""" 
+# write a program to input 2 numbers and print thier sum
+
+first = int(input("enter first : "))
+second = int(input("enter second : "))
+
+print("sum = ", first + second)
+
+
+
+#WAP to input side of square and print its area
+
+side = float(input("enter square side : "))
+
+print("area : ", side * side)
+print("area : ", side **2)
+
+
+
+#WAP to input 2 floating point numbers and print thier averages:
+
+a = float(input("enter first : "))
+b = float(input("enter second : "))
+
+print("avg", (a+b)/2)
+
+"""
+
+
+# WAP to input 2 int numbers, a and b. Print True if a is greater than or equal to b, If not print False
+
+a = int(input("enter first : "))
+b = int(input("enter second : "))
+print(a >= b)
---
+++ type conversion/conversion.py
@@ -0,0 +1,27 @@
+# type conversion
+a = 2
+b = 4.25
+
+sum = a + b
+print(sum)
+
+
+
+
+#type casting
+a,b = 1, "2"
+c = int(b) 
+sum = a + c
+
+print(type(a))
+print(sum)
+
+
+
+#str
+a = 3.14
+a = str(a)
+
+print(type(a))
+
+
